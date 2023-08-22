d7c1f5654f48d984ac5ad8c6cbb5cc4b587c5cdc
David Ellis <david.ellis94@gmail.com>
2023-08-22 21:16:52 +0100

    converted into gif

--- python/temp-spiral.py
+++ python/temp-spiral.py
@@ -30,20 +30,22 @@ def TempR(temp):
     return temp + 1
 
 def setup():
-    fig = plt.figure(figsize = (7,7))
-    
     theta = np.linspace(-np.pi/2, 8/6*np.pi, 12)
-    label_x = 3.5 * np.cos(-theta)
-    label_y = 3.5 * np.sin(-theta)
+    label_x = 3.3 * np.cos(-theta)
+    label_y = 3.3 * np.sin(-theta)
+    
+    
+    plt.axis([-4,4,-4,4])
+    plt.xticks([])
+    plt.yticks([])
+    
     for i in range(12):
         plt.annotate(months[i], 
                      (label_x[i], label_y[i]), 
                      size = 16, ha='center', 
                      va='center',
                      rotation = -i/12*360)
-        plt.axis([-4,4,-4,4])
-    plt.xticks([])
-    plt.yticks([])
+
     # plot 0, 1, 2 degree circles
     for temp in [0, 1, 2]:
         theta = np.linspace(0, 2*np.pi, 100)
@@ -57,29 +59,16 @@ def setup():
         plt.annotate(label, (0,TempR(temp)),
                      ha = "center", va = "center",
                      bbox = bbox, zorder = 0)
+        
+#%% Visualisation 
 
-    return fig
-
-setup()
-
-#%% Visualisation
-
-R = TempR(all_temps)
-num_years = 144
-theta = np.linspace(-np.pi/2, num_years*8/6*np.pi, num_years*12)
-
-plt.close("all")
-
-setup()
-
-year = plt.annotate(str(600//12 + 1880), (0,0), ha = "center", va = "center",
-                 bbox = dict(fc="white", lw = 0), size = 16)
-for i in range(600,len(theta)):
+def frame(frame_num, year):        
+    index = frame_num
+    R_pair_i = R[index:index+2]
+    theta_pair_i = np.array([-np.pi/2 + frame_num * np.pi/6,
+                    -np.pi/2 + (frame_num + 1) * np.pi/6])
     
-    R_pair_i = R[i:i+2]
-    theta_pair_i = theta[i:i+2]
-    
-    avgTemp = np.mean(all_temps[i:i+2])
+    avgTemp = np.mean(all_temps[index:index+2])
     
     if avgTemp >= 0:
         red = 0.8
@@ -92,10 +81,47 @@ for i in range(600,len(theta)):
     
     x_pair_i = R_pair_i*np.cos(-theta_pair_i)
     y_pair_i = R_pair_i*np.sin(-theta_pair_i)
-    plt.plot(x_pair_i, y_pair_i, zorder = 1,
-             color = (red, green, blue))
-    year.remove()
-    year = plt.annotate(str(i//12 + 1880), (0,0), ha = "center", va = "center",
-                 bbox = dict(fc="white", lw = 0), size = 16)
     
-    plt.pause(0.001)
+    ax.plot(x_pair_i, y_pair_i, zorder = 1,
+          color = (red, green, blue))
+    year.remove()
+    year = ax.annotate(str(index//12 + 1880), (0,0), 
+                  ha = "center", va = "center",
+                  bbox = dict(fc="white", lw = 0), size = 16)
+
+    return(year)
+    
+fig = plt.figure(figsize = (7,7))
+ax = fig.add_subplot(111)
+
+setup()
+
+R = TempR(all_temps)
+num_frames = 1000
+num_years = 144
+skip = 144*4
+
+year = ax.annotate(str(0) , (0,0), 
+                  ha = "center", va = "center",
+                  bbox = dict(fc="white", lw = 0), size = 16)
+ax.set_title("Global-mean monthly temperature (L-OTI)", size = 16)
+
+for i in range(skip, 1726):
+    year = frame(i, year)
+    plt.savefig("../figures/spiral/{}.png".format(i), 
+                bbox_inches = "tight")
+
+
+#%% Convert to gif
+
+from PIL import Image
+
+images = []
+
+for i in range(skip, 1726):
+    im = Image.open("../figures/spiral/{}.png".format(i))
+    images.append(im)
+
+#%%
+images[0].save('../figures/spiral.gif',
+               save_all=True, append_images=images[1:], optimize=False, duration=40, loop=0)
